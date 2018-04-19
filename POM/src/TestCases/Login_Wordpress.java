fa28ed6e4f4103fbd07fca0aa95e34346af739c7
Kritika <Kritika>
2018-04-20 03:12:06 +1000

    Third commit

--- POM/src/TestCases/Login_Wordpress.java
+++ POM/src/TestCases/Login_Wordpress.java
@@ -7,6 +7,7 @@ public class Login_Wordpress {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// to check commit
 		
 	//	LoginPageObjects login = new LoginPageObjects();
 	//	login.Login("", Pass);
